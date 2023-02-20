cb609328a3803d8819e6b0d45b47046bb7de20e0
saichandu <saichandu@gmail.com>
2023-02-20 18:30:13 +0530

    part 3

--- app/src/main/java/com/example/c/activities/MainActivity.java
+++ app/src/main/java/com/example/c/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.c;
+package com.example.c.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.c.R;
 
 public class MainActivity extends AppCompatActivity {
 
