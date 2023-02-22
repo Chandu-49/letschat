10c3409812dc69168f2daff0ece82affc42f6529
saichandu <saichandu@gmail.com>
2023-02-22 22:23:28 +0530

    PART 4(Final checked out origin branch and merged part 4 to it

--- app/src/main/java/com/example/c/activities/SignInActivity.java
+++ app/src/main/java/com/example/c/activities/SignInActivity.java
@@ -40,7 +40,6 @@ public class SignInActivity extends AppCompatActivity {
             }
 
         });
-        System.out.println("hiii");
     }
     private void showToast(String message){
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
